5451dc1c8e2a9dd91df94696a953233c4478c2b7
matt-sys <54177999+matt-sys@users.noreply.github.com>
2019-08-23 09:52:30 -0300

    Add files via upload

--- practico_4/main.cpp
+++ practico_4/main.cpp
@@ -1,15 +1,40 @@
+
+//------------------------------------------------------------//
+
 #include <iostream>
 
-#include "./lib/comparar_arreglo_iterativo.hpp"
+/// #include "./lib/intercambiar.hpp"
+
+#include "./lib/mostrar_arreglo.hpp"
+
+#include "./lib/comparar_iterativo.hpp"
+#include "./lib/comparar_recursivo.hpp"
+
+#include "./lib/ocurrencias_iterativo.hpp"
+#include "./lib/ocurrencias_recursivo.hpp"
+
+/// #include "./lib/invertir_iterativo.hpp"
+/// #include "./lib/invertir_recursivo.hpp"
+
+#include "./lib/minimo_iterativo.hpp"
+/// #include "./lib/minimo_recursivo.hpp"
+
+#include "./lib/maximo_iterativo.hpp"
+/// #include "./lib/maximo_recursivo.hpp"
+
+#include "./lib/total_iterativo.hpp"
+#include "./lib/total_recursivo.hpp"
+
+//------------------------------------------------------------//
 
 int main()
 {
-    int
-    cout << "Hello world!" << endl;
-    return 0;
 
+  constexpr int largo = 5;
+            int arreglo [ largo ];
 
+  Mostrar_Arreglo( arreglo, largo );
 
 }
 
-
+//----------------------------------------------------------//
