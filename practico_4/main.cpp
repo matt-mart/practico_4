059ea246e8603b33d6e07c7c4eb1e8bd508a055e
matt-sys <54177999+matt-sys@users.noreply.github.com>
2019-08-25 13:37:21 -0300

    Add files via upload

--- practico_4/main.cpp
+++ practico_4/main.cpp
@@ -11,39 +11,50 @@
 #include "./lib/comparar_recursivo.hpp"
 //
 #include "./lib/ocurrencias_iterativo.hpp"
-////#include "./lib/ocurrencias_recursivo.hpp"
+#include "./lib/ocurrencias_recursivo.hpp"
 //
 #include "./lib/invertir_iterativo.hpp"
 #include "./lib/invertir_recursivo.hpp"
 //
 #include "./lib/minimo_iterativo.hpp"
-///// #include "./lib/minimo_recursivo.hpp"
+//#include "./lib/minimo_recursivo.hpp"
 //
 #include "./lib/maximo_iterativo.hpp"
-///// #include "./lib/maximo_recursivo.hpp"
+//#include "./lib/maximo_recursivo.hpp"
 //
 #include "./lib/total_iterativo.hpp"
 #include "./lib/total_recursivo.hpp"
+
+#include "./lib/factorial_iterativo.hpp"
+#include "./lib/factorial_recursivo.hpp"
 
 //------------------------------------------------------------//
 
 int main()
 {
 
-  constexpr int largo = 5;
-            int arreglo [ largo ];
-        int n=0;
+//int num;
+//std::cout << "ingrese un numero " << std::endl;
+//std::cin >> num;
+//std::cout << Factorial_Recursivo(num) << std::endl;
 
-std::cout << " Array normal: " << std::endl;
-Mostrar_Arreglo( arreglo, largo );
-std::cout<< " "<<std::endl;
-Invertir_Recursivo(arreglo, largo);
+//Estaría bien hacer un menú con todas las opciones.
 
-std::cout << " Array invertido: " << std::endl;
-
-Mostrar_Arreglo( arreglo, largo );
-
-return 0;
+//
+//  constexpr int largo = 5;
+//            int arreglo [ largo ];
+//        int n=0;
+//
+//std::cout << " Array normal: " << std::endl;
+//Mostrar_Arreglo( arreglo, largo );
+//std::cout<< " "<<std::endl;
+//Invertir_Recursivo(arreglo, largo);
+//
+//std::cout << " Array invertido: " << std::endl;
+//
+//Mostrar_Arreglo( arreglo, largo );
+//
+//return 0;
 
 //----------------------------------------------------------//
 
